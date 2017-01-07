8664a84d00c7c0b46e77b51aa0fd8df150a67f2a
Jeffrey Pfau <jeffrey@endrift.com>
2017-01-06 19:08:44 -0800

    Add an interface!

--- Makefile
+++ Makefile
@@ -4,9 +4,11 @@ install: phreakium-z.gb
 	ems-flasher --format
 	ems-flasher --write $<
 
+phreakium-z.o: symbols.bin font.bin
+
 %.o: %.s
 	rgbasm -o $@ $<
 
 phreakium-z.gb: phreakium-z.o
-	rgblink -o $@ $<
+	rgblink -o $@ -n $(patsubst %.o,%.sym,$<) $<
 	rgbfix -vp0 $@
--- phreakium-z.s
+++ phreakium-z.s
@@ -28,7 +28,40 @@ rWave_e     EQU $ff3e
 rWave_f     EQU $ff3f
 rLCDC       EQU $ff40
 rSTAT       EQU $ff41
+rSCY        EQU $ff42
+rSCX        EQU $ff43
+rDMA        EQU $ff46
+rBGP        EQU $ff47
+rOBP0       EQU $ff48
+rOBP1       EQU $ff49
 rIE         EQU $ffff
+
+size_font	  EQU $0600
+size_symbols  EQU $0200
+tilemap       EQU $9800
+
+name      EQUS "Phreakium-Z"
+sq_0      EQU $00
+arrow_u   EQU $01
+arrow_d   EQU $02
+arrow_r   EQU $03
+arrow_l   EQU $04
+line_h    EQU $05
+line_v    EQU $06
+corner_tl EQU $07
+corner_tr EQU $08
+corner_br EQU $09
+corner_bl EQU $0a
+tee_u     EQU $0b
+tee_r     EQU $0c
+tee_d     EQU $0d
+tee_l     EQU $0e
+sq_1      EQU $0f
+sq_2      EQU $10
+sq_3      EQU $11
+
+disp_y    EQU $50
+disp_x    EQU $2c
 
 SECTION "vblank",HOME[$40]
 	jp vblank
@@ -44,24 +77,64 @@ _start:
 	xor a
 	ldio [rNR52], a
 
-	ld a, $80
-	ldio [rLCDC], a
-	ld a, $01
-	ldio [rIE], a
-	ld a, $10
-	ldio [rSTAT], a
-
 	ld hl, active_slot
 	ld [hl], $0
 
 	ld hl, ramtable
 	ld bc, firez
-copy_table:
+.copy_table:
 	ld a, [bc]
 	inc bc
 	ld [hl+], a
 	or a
-	jr nz, copy_table
+	jr nz, .copy_table
+
+	xor a
+	ldio [rLCDC], a
+
+	ld hl, symbols
+	ld bc, tiles
+	ld de, size_symbols + size_font
+	call copy
+
+	ld hl, header
+	ld bc, tilemap
+	ld de, end_header - header
+	call copy
+
+	ld hl, tilemap+end_header - header
+	ld bc, $03C0
+	call zero
+
+	ld hl, oam_dma_rom
+	ld bc, oam_dma
+	ld de, end_oam_dma_rom - oam_dma_rom
+	call copy
+
+	ld hl, oam
+	ld bc, $a0
+	call zero
+
+	ld hl, header_oam
+	ld bc, oam
+	ld de, end_header_oam - header_oam
+	call copy
+	call oam_dma
+
+	ld a, $c4
+	ldio [rBGP], a
+	ld a, $d4
+	ldio [rOBP0], a
+	ld a, $d0
+	ldio [rOBP1], a
+	ld a, $04
+	ldio [rSCX], a
+	ld a, $93
+	ldio [rLCDC], a
+	ld a, $01
+	ldio [rIE], a
+	ld a, $10
+	ldio [rSTAT], a
 
 	ei
 runloop:
@@ -72,13 +145,31 @@ runloop:
 	call nz, play_ramtable
 	jr runloop
 
+copy:
+	ld a, [hl+]
+	ld [bc], a
+	inc bc
+	dec de
+	ld a, d
+	or a, e
+	jr nz, copy
+	ret
+
+zero:
+	xor a
+	ld [hl+], a
+	dec bc
+	ld a, b
+	or a, c
+	jr nz, zero
+	ret
+
 play_ramtable:
 	push hl
 	ld hl, ramtable
 	call play_table
 	pop hl
 	ret
-
 
 wait_sec:
 	ld b, a
@@ -183,13 +274,16 @@ next_table:
 	reti
 
 vblank:
+	push hl
+	push bc
+	push de
 	call read_buttons
 	ld b, a
 	ld a, [down_buttons]
 	and a, b
 	ld a, b
 	ld [down_buttons], a
-	jr nz, .reti
+	jp nz, .reti
 	bit 4, b
 	call nz, cursor_right
 	bit 5, b
@@ -198,8 +292,109 @@ vblank:
 	call nz, cursor_increase
 	bit 7, b
 	call nz, cursor_decrease
+
+	ld bc, ramtable
+	ld a, [bc]
+	inc bc
+	add sp, -$2
+	ld hl, sp 0
+	call write_hex
+	pop de
+	ld hl, oam + end_header_oam - header_oam
+
+	ld [hl], disp_y + $10
+	inc hl
+	ld [hl], disp_x
+	inc hl
+	ld a, e
+	ld [hl+], a
+	ld [hl], 0
+	inc hl
+
+	ld [hl], disp_y + $10
+	inc hl
+	ld [hl], disp_x + 8
+	inc hl
+	ld a, d
+	ld [hl+], a
+	ld [hl], 0
+	inc hl
+
+	ld a, [active_slot]
+	add a, a
+	add a, a
+	add a, a
+	ld d, a
+	add a, a
+	add a, d
+	add a, disp_x + $1c
+
+	ld [hl], disp_y + $18
+	inc hl
+	ld [hl], a
+	inc hl
+	ld [hl], arrow_d
+	inc hl
+	ld [hl], 0
+	inc hl
+
+	ld [hl], disp_y + $8
+	inc hl
+	ld [hl], a
+	inc hl
+	ld [hl], arrow_u
+	inc hl
+	ld [hl], 0
+	inc hl
+
+	call oam_dma
+
+	ld hl, table_text
+	ld a, [bc]
+	inc bc
+	call write_hex
+	xor a
+	ld [hl+], a
+	ld a, [bc]
+	inc bc
+	call write_hex
+	xor a
+	ld [hl+], a
+	ld a, [bc]
+	inc bc
+	call write_hex
+	ld hl, table_text
+	ld bc, tilemap + disp_y * 4 + disp_x / 8 + 3
+	ld de, $8
+	call copy
+
 .reti:
+	pop de
+	pop bc
+	pop hl
 	reti
+
+write_hex:
+	push bc
+	ld b, a
+	swap a
+	and a, $f
+	or a, $30
+	cp a, $3a
+	jr c, .a
+	add a, $7
+.a:
+	ld [hl+], a
+	ld a, b
+	and a, $f
+	or a, $30
+	cp a, $3a
+	jr c, .b
+	add a, $7
+.b:
+	ld [hl+], a
+	pop bc
+	ret
 
 read_buttons:
 	ld a, $20
@@ -279,23 +474,93 @@ table:
 	dw 0
 
 firez:
-	db $c6
-	db $ca
-	db $d0
-	db $ce
-	db 0
+	db $c6, $ca, $d0, $ce, 0
 
 waterium:
-	db $c6
-	db $ce
-	db $d0
-	db $c9
-	db 0
+	db $c6, $ce, $d0, $c9, 0
+
+psychium:
+	db $c6, $ce, $d1, $cb, 0
+
+header:
+REPT ($14 - strlen("{name}")) / 2
+	db sq_1
+ENDR
+
+	db corner_tl
+REPT strlen("{name}")
+	db line_h
+ENDR
+	db corner_tr
+REPT $1e - strlen("{name}")
+	db sq_1
+ENDR
+
+	db line_v, "{name}", line_v
+REPT ($12 - strlen("{name}")) / 2 + $c
+	db sq_1
+ENDR
+
+REPT ($13 - strlen("{name}")) / 2
+	db line_h
+ENDR
+	db tee_u
+REPT strlen("{name}")
+	db line_h
+ENDR
+	db tee_u
+REPT ($13 - strlen("{name}")) / 2
+	db line_h
+ENDR
+end_header:
+
+header_oam:
+	db $20, $4 + ($13 - strlen("{name}")) * 4
+	db tee_u, $0
+
+	db $20, $c + ($13 + strlen("{name}")) * 4
+	db tee_u, $0
+
+	db $18, $c + ($13 + strlen("{name}")) * 4
+	db line_v, $10
+
+	db $18, $c + ($13 + strlen("{name}")) * 4
+	db line_v, $10
+end_header_oam:
+
+oam_dma_rom:
+	ld a, oam / $100
+	ld [rDMA], a
+	ld a, $28
+.loop
+	dec a
+	jr nz, .loop
+	ret
+end_oam_dma_rom
 
 SECTION "ram",BSS
+oam:
+	ds $a0
 ramtable:
 	ds 5
 active_slot:
 	ds 1
 down_buttons:
 	ds 1
+table_text:
+	ds 8
+
+SECTION "tilerom",HOME
+symbols:
+INCBIN "symbols.bin"
+font:
+INCBIN "font.bin"
+
+SECTION "tiles",VRAM[$8000]
+tiles:
+	ds size_symbols
+	ds size_font
+
+SECTION "oam_dma",HRAM
+oam_dma:
+	ds end_oam_dma_rom - oam_dma_rom
